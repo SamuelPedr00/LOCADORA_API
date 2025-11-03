695995e5415cf9c24659f193d05a9fc3217983db
Samuel Pedro <samuelpedro.ctt@gmail.com>
2025-11-03 19:59:12 -0300

    Iniciando Projeto
    criando projeto e as respectivas migrations, models e controllers para crud de api

---
+++ app_locadora_carros/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('cliente', App\Http\Controllers\ClienteController::class);
+Route::apiResource('locacao', App\Http\Controllers\LocacaoController::class);
+Route::apiResource('marca', App\Http\Controllers\MarcaController::class);
+Route::apiResource('modelo', App\Http\Controllers\ModeloController::class);
