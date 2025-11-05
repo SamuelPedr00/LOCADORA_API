3ef2e2cd41de79778b6bf6234272875fc67662d7
Samuel Pedro <samuelpedro.ctt@gmail.com>
2025-11-04 22:39:42 -0300

    relacionando pesquisa

--- app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -19,7 +19,7 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        return response()->json($this->modelo->all(), 200);
+        return response()->json($this->modelo->with('marca')->get(), 200);
     }
 
     /**
@@ -63,7 +63,7 @@ class ModeloController extends Controller
      */
     public function update(Request $request, int $id)
     {
-        $modelo = $this->modelo->find($id);
+        $modelo = $this->modelo->with('marca')->find($id);
         if ($modelo == null) {
             return response()->json(['error' => 'Pesquisa não encontrada'], 404);
         }
